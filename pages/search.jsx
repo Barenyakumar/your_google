b39863aeb18b9fd62c1d5b5cf26608d335661fae
BarenyaKumar <barenyakumar@gmail.com>
2023-01-04 22:00:01 +0530

    get links from google

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import Response from "../Response";
 
-export default function search() {
+export default function search({ results }) {
+  console.log(results);
   return (
     <div>
       {/* head */}
@@ -17,4 +19,20 @@ export default function search() {
 
     </div>
   )
+}
+
+//getServerSideProps i built in function , context is similar to router but used for server side
+export async function getServerSideProps(context) {
+  const mockData = true
+  const data = mockData ? Response : 
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyA5XzGzkO9xQ-phdOFTs192INxHweHfBNg
+&cx=d562a6696b4a74ee0&q=${context.query.term}${
+      context.query.searchType && "&searchType=image"
+    }`).then((response) => response.json())
+  
+  return {
+    props: {
+      results : data
+    }
+  }
 }
